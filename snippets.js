caafa51c9610dc15f460491e1cce230a77815634
wabiss3 <dl592675334@gmail.com>
2026-05-19 10:57:01 +0800

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -1,7 +1,7 @@
 import { connect } from 'cloudflare:sockets';
 
-let proxyIP = 'proxy.xxxxxxxx.tk:50001';  // proxyIP，也可以是socks5或http
-let yourUUID = '93bf61d9-3796-44c2-9b3a-49210ece2585';  // UUID
+let proxyIP = 'kr.william.us.ci';  // proxyIP，也可以是socks5或http
+let yourUUID = 'c73313a2-9b65-475b-8d45-b63dfea4fa16';  // UUID
 
 // CDN 
 let cfip = [ // 格式:优选域名:端口#备注名称、优选IP:端口#备注名称、[ipv6优选]:端口#备注名称、优选域名#备注 
